7b8354007ca73adfdbabb195f78120f132e50736
Shoaib <87575441+Shoeb02@users.noreply.github.com>
2024-01-08 16:14:48 +0530

    Add files via upload

---
+++ edith.py
@@ -0,0 +1,241 @@
+   
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser as wb
+import os
+import smtplib
+import requests
+from pprint import pprint
+from selenium import webdriver
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+#print(voices[1].id)
+engine.setProperty('voice',voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    speak("Turning on Powers Server Online")
+    speak("Welcome back!")
+    hour = int(datetime.datetime.now().hour)
+    print(hour)
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date = int(datetime.datetime.now().day)
+    Time = datetime.datetime.now().strftime("%I:%M:%S") 
+    # print(Time)
+    # print(date)
+    # print(month)
+    # print(year)
+    # speak("the current Time is")
+    # speak(Time)
+    # speak("the current Date is")
+    # speak(date)
+    # speak(month)
+    # speak(year)
+    if hour>=6 and hour<12:
+        speak("Good Morning! Shoaib")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!Shoaib")
+
+    elif hour>=18 and hour<24:
+        speak("Good Evening! Shaoib")
+
+    else:
+        speak("Good Night! Shoaib")
+
+    speak(" edith at your service! Please tell me how can I help you?")
+#wishMe()
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said:{query}\n")
+
+    except Exception as e:
+        print(e)
+        print("Please say that again...")
+        speak("Please say that again...")
+        return "None"
+    return query
+
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('senderemail@gmail.com', 'Password')
+    server.sendmail('senderemail@gmail.com', to, content)
+    server.close()
+
+def lighton():
+    driver = webdriver.Chrome('C:/Users/Username/Downloads/chromedriver.exe')
+    driver.get("https://Add here.000webhostapp.com/main.html")
+    elem1 = driver.find_element_by_id("S1off")
+    elem1.click()
+
+def lightoff():
+    driver = webdriver.Chrome('C:/Users/manbirmarwah/Downloads/chromedriver.exe')
+    driver.get("https://Add here.000webhostapp.com/main.html")
+    #Add the webhost name
+    elem1 = driver.find_element_by_id("S1on")
+    elem1.click()
+            
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'search in chrome' in query:
+            speak("what should i search?")
+            chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+            #Add the Location of the chrome browser
+
+            r = sr.Recognizer()
+
+            with sr.Microphone() as source:
+                print('Say something!')
+                audio = r.listen(source)
+                print("done")
+            try:
+                text = r.recognize_google(audio)
+                print('Google thinks you said: \n' +text +'.com')
+                wb.get(chrome_path).open(text+'.com')
+            except Exception as e:
+                print(e)
+        
+        elif 'how is the weather' and 'weather' in query:
+
+            url = 'https://api.openweathermap.org/'#Open api link here
+
+            res = requests.get(url)
+
+            data = res.json()
+
+            weather = data['weather'] [0] ['main'] 
+            temp = data['main']['temp']
+            wind_speed = data['wind']['speed']
+
+            latitude = data['coord']['lat']
+            longitude = data['coord']['lon']
+
+            description = data['weather'][0]['description']
+            speak('Temperature : {} degree celcius'.format(temp))
+            print('Wind Speed : {} m/s'.format(wind_speed))
+            print('Latitude : {}'.format(latitude))
+            print('Longitude : {}'.format(longitude))
+            print('Description : {}'.format(description))
+            print('weather is: {} '.format(weather))
+            speak('weather is : {} '.format(weather))
+
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%I:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+        
+        elif 'the date' in query:
+            year = int(datetime.datetime.now().year)
+            month = int(datetime.datetime.now().month)
+            date = int(datetime.datetime.now().day)
+            speak("the current Date is")
+            speak(date)
+            speak(month)
+            speak(year)
+
+
+        elif 'email to harry' and 'send email' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "ReciversEmail@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("I'm sorry. I am not unable to send this email.")      
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\user account\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"#ADD THE PATH OF THE PROGEM HERE
+            os.startfile(codePath)
+
+
+        elif 'open' in query:
+            os.system('explorer C://{}'.format(query.replace('Open','')))
+
+        
+        elif 'turn on lights' in query:
+            speak("Turning on the lights.")
+            lighton()
+            speak("Lights are on")
+        
+        elif 'turn off lights' in query:
+            speak("OK,sir turning off the Lights")
+            lightoff()
+            speak("Lights are off")
+            
+        elif 'it\'s my birthday today' in query:
+            print(" Wow! Wish you a very Happy Birthday")
+            speak(" Wow! Wish you a very Happy Birthday")
+    
+        elif "where is" in query:
+            data = query.split(" ")
+            location = data[2]
+            speak("Hold on, I will show you where " + location + " is.")
+            os.system('cmd /k "start chrome https://www.google.nl/maps/place/"'+ location)
+            os.system("start chrome https://www.google.nl/maps/place/" + location)  
+
+        #open youtube
+    #     elif 'jarvis open youtube' in query:
+    #      webbrowser.open("youtube.com")
+
+    #     #open google
+    #     elif 'jarvis open github' in query:
+    #         webbrowser.open("github.com")
+
+    #     #open stackoverflow
+    #     elif 'jarvis open stackoverflow' in query:
+    #         webbrowser.open("stackoverflow.com")
+
+    #    #time
+        elif 'jarvis what is the time now' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)  
+
+        elif 'go offline' in query:
+            speak("Shutting down the system")
+            speak("Bye BYe  ")
+            quit()
+        elif "instagram" in query:
+             os.system('cmd /k "start chrome https://www.instagram.com/ft.shoaiib"')
+        elif "who i am" in query:
+            speak(" Your are my Chief Shoaib Sir  i'm glad to be your assistant")
+        
